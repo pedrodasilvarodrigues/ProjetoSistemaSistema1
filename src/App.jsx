39c1369cdb07dd393a5eb9052a9528f46986b0d0
Pedro da Silva Rodrigues <0001083315@senaimgaluno.com.br>
2026-03-02 17:26:20 -0300

    criacao de arquivo em react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Clientes } from './Pages/Clientes';
 import { Produtos } from './Pages/Produtos/';
+
 import { Toaster } from 'sonner';
 import { Vendas } from './Pages/Vendas';
+import { Fornecedor } from './Pages/Fornecedor/indexFornecedor';
 
 export default function App() {
   return (
@@ -11,7 +13,9 @@ export default function App() {
       <nav className="bg-green-800 p-4 text-white flex gap-4">
         <Link to="/" className="hover:underline">Clientes</Link>
         <Link to="/produtos" className="hover:underline">Produtos/Estoque</Link>
+           <Link to="/fornecedores" className="hover:text-green-200 transition-colors">Fornecedores</Link>
         <Link to="/vendas" className="hover:text-green-200 transition-colors">Vendas/Financeiro</Link>
+     
         
       </nav>
 
@@ -19,6 +23,7 @@ export default function App() {
         <Route path="/" element={<Clientes />} />
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/vendas" element={<Vendas />} />
+        <Route path="/fornecedores" element={<Fornecedor />} />
       </Routes>
     </BrowserRouter>
   );
